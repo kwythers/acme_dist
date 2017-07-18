9c992b480120425bba671ba4e027be407d7979e1
Kirk Wythers <kwythers@umn.edu>
2017-07-18 16:04:53 -0500

    commented out un-needed libraries

--- ncp_process.R
+++ ncp_process.R
@@ -1,17 +1,18 @@
 # Load some libraries
 #library(rPython)
 #library(RNetCDF)
-library(tidyverse)
+#library(tidyverse)
 library(ncdf4)
-library(data.table)
+#library(data.table)
 library(plyr)
 library(ggplot2)
-library(grid)
-library(gridExtra)
-library(zoo)
-library(scales)
+#library(grid)
+#library(gridExtra)
+#library(zoo)
+#library(scales)
 #path <- "ACME/source/run/trait_dist/trait_dist2_US-UMB_I20TRCLM45CBCN/run"
-path <- "ACME/source/run/trait_dist"
+#path <- "ACME/source/run/trait_dist"
+path <- "~/../chen1718/ACME/source/run/"
 #filenames <- list.files(path,full.names = TRUE,pattern = "trait_dist*",recursive = TRUE)
 filenames <- list.files(path,full.names = TRUE,pattern = "trait_dist[0-9]_US-UMB_I20TRCLM45CBCN.clm2.h0.+.nc",recursive = TRUE)
 #grep(pattern = "trait_dist[0-9]_US-UMB_I20TRCLM45CBCN.clm2.h0.+.nc",filenames,value = TRUE,perl = TRUE)
