66df1cfc81c559ccd092dedca212310201512a19
Kirk R. Wythers <kwythers@umn.edu>
2017-09-22 10:56:06 -0500

    Pulling data from 14 fluxnet sites

--- fluxnet_get_data.R
+++ fluxnet_get_data.R
@@ -1,34 +1,82 @@
 ##### Load some libraries
 
-#library(rPython)
-#library(RNetCDF)
-library(tidyverse)
-#library(ncdf4)
-library(data.table)
-library(plyr)
-library(ggplot2)
-library(grid)
-library(gridExtra)
-#library(zoo)
-library(scales)
+require(tidyverse)
+require(stringr)
+require(readr)  # for read_csv()
+require(dplyr)  # for mutate()
+require(tidyr)  # for unnest()
+require(purrr)  # for map(), reduce()
+require(lubridate)
 
 readpath <- function()
 { 
-  n <- readline(prompt="Enter path to nc files without starting or finishing slash (e.g. ~/Google Drive/globland/data/fluxnet/) : ")
+  n <- readline(prompt = "Enter path to nc files without starting or finishing slash :")
   return(as.character(n))
 }
 
 readfilename <- function()
 { 
-  n <- readline(prompt="Enter file or file pattern : ")
+  n <- readline(prompt = "Enter file or file pattern : ")
   return(as.character(n))
 }
 
-#Current path ~/../shared/fluxnet/fluxnet/15_site_fluxnet_data/
-#Current path ~/Google Drive\\data/fluxnet/15_site_fluxnet_data/
+#Current path ~/../shared/fluxnet/fluxnet ### at MSI
+#Current path ~/projects/globland/fluxnet ### local disk
+
 path <- print(readpath())
-#current file pattern FLX_[A-Z]{2}-[a-zA-Z0-9]{3}FLUXNET2015*[0-9]{4}-[0-9]{4}_[0-9]-[0-9].csv
+
+# FLX_[A-Z]{2}-[A-Za-z0-9]{3}_FLUXNET2015_[A-Z]{4,7}_[A-Z]{2}_[0-9]{4}-[0-9]{4}_[0-9]{1}-[0-9]{1}.csv ## everything
+# FLX_[A-Z]{2}-[A-Za-z0-9]{3}_FLUXNET2015_FULLSET_DD_[0-9]{4}-[0-9]{4}_[0-9]{1}-[0-9]{1}.csv ### FULLSET DD files only
+# FLX_[A-Z]{2}-[A-Za-z0-9]{3}$ #### sitename pattern
+
 fileptrn <- print(readfilename())
-#filenames <- list.files(path,full.names = TRUE,pattern = "trait_dist*",recursive = TRUE)
+
 filenames <- list.files(path, full.names = TRUE, pattern = fileptrn, recursive = TRUE)
-#grep(pattern = "trait_dist[0-9]_US-UMB_I20TRCLM45CBCN.clm2.h0.+.nc",filenames,value = TRUE,perl = TRUE)
+
+# site <- str_extract(filenames, "FLX_[A-Z]{2}-[A-Za-z0-9]{3}_FLUXNET2015_[A-Z]{4,7}_[A-Z]{2}_[0-9]{4}-[0-9]{4}_[0-9]{1}-[0-9]{1}.csv")
+# site <- str_extract(filenames, "[A-Z]{2}-[A-Za-z0-9]{3}")
+
+##### run through all data directories, add column for SITE and fill with regular eppression
+data <- tibble(File = filenames) %>%
+  extract(File, "SITE", "([A-Z]{2}-[A-Za-z0-9]{3})", remove = FALSE) %>%
+  mutate(Data = lapply(File, read_csv)) %>%
+  unnest(Data) %>%
+  select(-File)
+
+# tbl <- lapply(filenames, read_csv) %>% 
+#   bind_rows()
+
+# separate "timestamp" into date pieces and create a "DATE" column
+data1 <- separate(data, TIMESTAMP, into = c("YEAR", "MONTHDAY"), sep = 4)
+data2 <- separate(data1, MONTHDAY, into = c("MONTH", "DAY"), sep = 2)
+data3 <-unite(data2, "DATE", YEAR, MONTH, DAY, sep = "-", remove = FALSE)
+
+##### Grouping for figures
+# GPP by site and year
+gpp_siteyear <-data3 %>%
+  group_by(YEAR,SITE) %>%
+  dplyr::summarize(gpp_annual=sum(GPP_DT_VUT_USTAR50, na.rm=T)) %>%
+  ungroup() %>% # -- Here's an example of why you need to ungroup! --
+  dplyr::arrange(SITE)
+
+# # Global harvest by species category
+# spcatch <- d %>%
+#   group_by(year,spgroupname) %>%
+#   dplyr::summarize(totalcatch=sum(catch, na.rm=T)) %>%
+#   ungroup() %>% 
+#   arrange(spgroupname)
+# 
+# # USA harvest by species category over time
+# usa<- d %>%
+#   filter(country=='United States of America') %>%
+#   group_by(year,country,spgroupname) %>%
+#   dplyr::summarize(totalcatch=sum(catch,na.rm=T)) %>%
+#   ungroup() %>%
+#   arrange(spgroupname)
+
+##### plot some stuff
+g <- ggplot(gpp_siteyear, aes(class)) %>%
+g + geom_bar(gpp_by_site, aes(SITE))
+
+
+
