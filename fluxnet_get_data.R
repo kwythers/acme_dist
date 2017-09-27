b70b19d07efb338f5d5e4bae037d0809395e7046
Kirk R. Wythers <kwythers@umn.edu>
2017-09-26 21:40:04 -0500

    changed structure and classes to match plotting meeds with mutate() and mutate_if and mutate_at

--- fluxnet_get_data.R
+++ fluxnet_get_data.R
@@ -45,7 +45,8 @@ data_dd3 <-unite(data_dd2, "DATE", YEAR, MONTH, DAY, sep = "-", remove = FALSE)
 
 ##### pull out 4 variables for analysis
 dat_dd <- select(data_dd3, SITE, DATE, YEAR, MONTH, DAY, NEE_CUT_REF, 
-                 NEE_CUT_REF_JOINTUNC, RECO_NT_CUT_REF, GPP_NT_CUT_REF) %>%
+                 NEE_CUT_REF_JOINTUNC, RECO_NT_CUT_REF, GPP_NT_CUT_REF) %>% 
+  mutate(DATE = as.Date(DATE)) %>% 
   mutate(NEE_CUT_REF = replace(NEE_CUT_REF, NEE_CUT_REF == "-9999", "NA")) %>% 
   mutate(NEE_CUT_REF_JOINTUNC = replace(NEE_CUT_REF_JOINTUNC, NEE_CUT_REF_JOINTUNC == "-9999", "NA")) %>% 
   mutate(RECO_NT_CUT_REF = replace(RECO_NT_CUT_REF, RECO_NT_CUT_REF == "-9999", "NA")) %>%
@@ -62,7 +63,7 @@ data_yy <- tibble(File = filenames_yy) %>%
   select(-File)
 
 dat_yy <- select(data_yy, SITE, TIMESTAMP, NEE_CUT_REF, 
-                 NEE_CUT_REF_JOINTUNC, RECO_NT_CUT_REF, GPP_NT_CUT_REF) %>%
+                 NEE_CUT_REF_JOINTUNC, RECO_NT_CUT_REF, GPP_NT_CUT_REF) %>% 
   mutate(NEE_CUT_REF = replace(NEE_CUT_REF, NEE_CUT_REF == "-9999", "NA")) %>% 
   mutate(NEE_CUT_REF_JOINTUNC = replace(NEE_CUT_REF_JOINTUNC, NEE_CUT_REF_JOINTUNC == "-9999", "NA")) %>% 
   mutate(RECO_NT_CUT_REF = replace(RECO_NT_CUT_REF, RECO_NT_CUT_REF == "-9999", "NA")) %>%
@@ -73,14 +74,14 @@ dat_yy <- select(data_yy, SITE, TIMESTAMP, NEE_CUT_REF,
 
 ##### Grouping for figures
 # daily GPP by site and year
-gpp_dd_siteyear <-dat_dd %>%
-  group_by(SITE,YEAR) %>%
-  dplyr::summarize(gpp_annual = sum(GPP_NT_CUT_REF, na.rm = TRUE)) %>%
-  ungroup() %>% # -- Here's an example of why you need to ungroup! --
-  dplyr::arrange(SITE)
+# gpp_dd_siteyear <-dat_dd %>%
+#   group_by(SITE,YEAR) %>%
+#   dplyr::summarize(gpp_annual = sum(GPP_NT_CUT_REF, na.rm = TRUE)) %>%
+#   ungroup() %>% # -- Here's an example of why you need to ungroup! --
+#   dplyr::arrange(SITE)
 
 # daily GPP by site and year
-gpp_yy_site <- dat_yy
+# gpp_yy_site <- dat_yy
 
 # # Global harvest by species category
 # spcatch <- d %>%
@@ -98,8 +99,63 @@ gpp_yy_site <- dat_yy
 #   arrange(spgroupname)
 
 ##### plot some stuff
-ggplot(data = gpp_yy_site, aes(x = SITE, y = GPP_NT_CUT_REF)) +
-  geom_bar(stat="identity")
+# Pull out one site and plot
+au_tum_yy <- filter(dat_yy, SITE == "AU-Tum")
+
+ggplot(au_tum_yy, aes(TIMESTAMP, GPP_NT_CUT_REF)) + 
+  geom_bar(stat = "identity") + 
+  expand_limits(y = 0) +
+  # labs(title ="GPP (NT_CUT_REF)", x = "year", y = "annual gpp") + # lable control
+  scale_y_discrete(name = "GPP", breaks = c("0","1000","2000","3000", "4000"), 
+                   labels = c("0","1000","2000","3000", "4000")) +
+  scale_x_discrete(name = "YEAR", breaks = c("2000","2005","2010", "2015"), 
+                   labels = c("2000","2005","2010", "2015"))
+  # theme(axis.text.x = element_text(angle = 90)) + # rotate tic text to verticle
+  # theme(axis.ticks = element_blank(), axis.text.y = element_blank()) # hide ticks and text
+
+ggplot(dat_yy, aes(TIMESTAMP, GPP_NT_CUT_REF)) + 
+  geom_col(position = "dodge") + 
+  facet_wrap(~ SITE, drop = TRUE, 
+             labeller = label_parsed) + 
+  labs(title ="GPP (NT_CUT_REF)", x = "year", y = "annual gpp") + # lable control
+  theme(axis.text.x = element_text(angle = 90)) + # rotate tic text to verticle
+  theme(axis.ticks = element_blank(), axis.text.y = element_blank()) # hide ticks and text
+
+# Pull out one site and plot
+au_tum_dd <- filter(dat_dd, SITE == "AU-Tum")
+
+ggplot(au_tum_dd, aes(DATE, GPP_NT_CUT_REF)) + 
+  geom_line() + 
+  labs(title ="GPP (NT_CUT_REF)", x = "DATE", y = "DAILY GPP")  # lable control
+  # theme(axis.text.x = element_text(angle = 90)) + # rotate tic text to verticle
+  # theme(axis.ticks = element_blank(), axis.text.y = element_blank()) # hide ticks and text
+
+ggplot(dat_dd, aes(DATE, GPP_NT_CUT_REF)) + 
+  geom_line() + 
+  facet_wrap(~ SITE) + 
+  labs(title ="GPP (NT_CUT_REF)", x = "DATE", y = "DAILY GPP") + # lable control
+  theme(axis.text.x = element_text(angle = 90)) + # rotate tic text to verticle
+  theme(axis.ticks = element_blank(), axis.text.y = element_blank()) # hide ticks and text
+
+# ggplot(data = dat_yy, aes(x = SITE, y = GPP_NT_CUT_REF)) +
+#   geom_col() +  # geom_col() is replacement for geom_bar(stat = "identity")
+#   # independent x-axis scale in each facet, 
+#   # drop absent factor levels (actually not required here)
+#   facet_wrap(~ TIMESTAMP, scales = "free_x", drop = TRUE) +
+#   # use named character vector to replace x-axis labels
+#   scale_x_discrete(labels = dat_yy[, setNames(as.character(SITE), GPP_NT_CUT_REF)]) + 
+#   # replace x-axis title
+#   xlab(NULL) +
+#   # rotate x-axis labels
+#   theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=.5))
+
+
+
++ 
+  facet_wrap(~ SITE, scale = 'free_x') + 
+  # rotate x-axis labels
+  theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=.5))
+
 
 
 
